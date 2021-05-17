0f3392c00ac79a90f3b501d3a12f4fbb078d862a
C:\Unity <na1yeon33@gmail.com>
2021-05-17 19:58:09 +0900

    Playercontroller

---
+++ 2D_Tutorial_2021_05_17/Assets/Script/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour // mono로 동작 , 유니티 기본 동작
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
