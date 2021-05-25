1ffc7a9f5bc97c3c6b9877c345c058b03018ae48
C:\Unity <na1yeon33@gmail.com>
2021-05-25 19:56:50 +0900

    방향전환, 오브젝트 삭제

--- 2D_Tutorial_2021_05_17/Assets/Script/BulletController.cs
+++ 2D_Tutorial_2021_05_17/Assets/Script/BulletController.cs
@@ -7,7 +7,18 @@ public class BulletController : MonoBehaviour
     Vector3 m_dir = Vector3.left;
     [SerializeField]
     float m_speed = 10f;
+    [SerializeField]
+    SpriteRenderer m_sprRenderer;
+    [SerializeField]
+    float m_lifetime = 3f;
+    float m_time;
 
+    public void SetBullet(Vector3 pos, Vector3 dir)
+    {
+        transform.position = pos;
+        m_dir = dir;
+        m_sprRenderer.flipY = dir == Vector3.right ? true : false;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +28,12 @@ public class BulletController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        m_time += Time.deltaTime;
+        if(m_time > m_lifetime)
+        {
+            m_time = 0f;
+            Destroy(gameObject);
+        }
         gameObject.transform.position += m_dir * m_speed * Time.deltaTime;
     }
 }
--- 2D_Tutorial_2021_05_17/Assets/Script/PlayerController.cs
+++ 2D_Tutorial_2021_05_17/Assets/Script/PlayerController.cs
@@ -12,16 +12,22 @@ public class PlayerController : MonoBehaviour // mono로 동작, 유니티 기�
     Animator m_animator;
     [SerializeField]
     SpriteRenderer m_sprRenderer;
+    [SerializeField]
+    GameObject m_bulletPrefab;
+    [SerializeField]
+    Transform m_firePos;
+
     Rigidbody2D m_rigidBody;
-    
-    // Start is called before the first frame update
-    void Start()
+
+    #region Animation Event Methods
+    void AnimEvent_CreateBullet()
     {
-        
-        m_animator = gameObject.GetComponent<Animator>();
-        m_sprRenderer = gameObject.GetComponent<SpriteRenderer>();
-        m_rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        var obj = Instantiate(m_bulletPrefab);
+        //obj.transform.position = m_firePos.position;
+        var bullet = obj.GetComponent<BulletController>();
+        bullet.SetBullet(m_firePos.position, transform.eulerAngles.y != 0 ? Vector3.right : Vector3.left);
     }
+    #endregion
 
     void KeyControll()
     {
@@ -41,31 +47,44 @@ public class PlayerController : MonoBehaviour // mono로 동작, 유니티 기�
         {
             m_dir = Vector3.left;
             m_animator.SetBool("IsMove", true);
-            m_sprRenderer.flipX = false;
-
+            //m_sprRenderer.flipX = false;
+            transform.eulerAngles = new Vector3(0f, 0f, 0f); // 각도 
         }
 
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             m_dir = Vector3.right;
             m_animator.SetBool("IsMove", true);
-            m_sprRenderer.flipX = true;
+            //m_sprRenderer.flipX = true;
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);           
         }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             m_animator.SetBool("IsShoot", true);
         }
+
         if (Input.GetKeyUp(KeyCode.Space))
         {
             m_animator.SetBool("IsShoot", false);
         }
         var stateInfo = m_animator.GetCurrentAnimatorStateInfo(0);
-        if(!stateInfo.IsName("Shoot"))
-        
+        if (!stateInfo.IsName("Shoot"))
 
-        
-        gameObject.transform.position += m_dir * m_speed * Time.deltaTime;
+
+
+            gameObject.transform.position += m_dir * m_speed * Time.deltaTime;
     }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        m_animator = gameObject.GetComponent<Animator>();
+        m_sprRenderer = gameObject.GetComponent<SpriteRenderer>();
+        m_rigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    
     //void FixedUpdate()
     //{
     //    m_rigidBody.AddForce(m_dir * m_speed, ForceMode2D.Force);
