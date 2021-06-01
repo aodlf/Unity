531fbfa644b253ce07ae637bd23c51d4708e74a5
C:\Unity <na1yeon33@gmail.com>
2021-06-01 19:57:24 +0900

    NGUI

--- 2D_Tutorial_2021_05_17/Assets/Script/TitleController.cs
+++ 2D_Tutorial_2021_05_17/Assets/Script/TitleController.cs
@@ -5,28 +5,36 @@ using UnityEngine;
 public class TitleController : MonoBehaviour
 {
     bool m_isToggleOn;
+    bool m_isSelectWeapon;
     string m_id = "아이디를 입력하세요";
-    string m_pass = "1234"; 
+    string m_pass = "1234";
+    int m_select;
+    string[] m_weaponList = new string[] { "단검", "장검", "양손검", "바스타드", "할버드", "로켓런쳐"  };
     void OnGUI()
     {
-        if(GUI.Button(new Rect((Screen.width - 150)/2, (Screen.height - 50)/2, 150, 50), "START"))
-        {
-            Debug.Log("Start game");
-        }
-        GUILayout.BeginArea(new Rect(10, Screen.height - 400, 200, 400), GUI.skin.box);
-        GUILayout.Space(20);
-        GUILayout.Button("START", GUILayout.Width(100), GUILayout.Height(60));
-        m_isToggleOn = GUILayout.Toggle(m_isToggleOn, "무적모드");
-        if(m_isToggleOn)
-        {
-            GUILayout.Label("무적모드 활성화");
-        }
-        m_id = GUILayout.TextField(m_id);
-        m_pass = GUILayout.PasswordField(m_pass, '*');
-        GUILayout.EndArea();
+        //if(GUI.Button(new Rect((Screen.width - 150)/2, (Screen.height - 50)/2, 150, 50), "START"))
+        //{
+        //    Debug.Log("Start game");
+        //}
+        //GUILayout.BeginArea(new Rect(10, Screen.height - 400, 200, 400), GUI.skin.box);
+        //GUILayout.Space(20);
+        //GUILayout.Button("START", GUILayout.Width(100), GUILayout.Height(60));
+        //m_isToggleOn = GUILayout.Toggle(m_isToggleOn, "무적모드");
+        //if(m_isToggleOn)
+        //{
+        //    GUILayout.Label("무적모드 활성화");
+        //}
+        //m_id = GUILayout.TextField(m_id);
+        //m_pass = GUILayout.PasswordField(m_pass, '*');
+        //GUILayout.EndArea();
 
-        GUILayout.BeginArea(new Rect(Screen.width - 400 - 10, Screen.height - 400, 200, 400), GUI.skin.box);
-        GUILayout.EndArea();
+        //GUILayout.BeginArea(new Rect(Screen.width - 200 - 10, Screen.height - 400, 200, 400), GUI.skin.box);
+        //m_isSelectWeapon = GUILayout.Toggle(m_isSelectWeapon, "무기선택", GUI.skin.button);
+        //if (m_isSelectWeapon)
+        //{
+        //    m_select = GUILayout.SelectionGrid(m_select, m_weaponList, 1);
+        //}
+        //GUILayout.EndArea();
     }
     // Start is called before the first frame update
     void Start()
